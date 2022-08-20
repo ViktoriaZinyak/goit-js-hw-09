5001dfb6f1f2e164919bdca7fbdb08ff4faa0de3
ViktoriyaZinyak <viktoriazinyak@gmail.com>
2022-08-20 19:04:13 +0300

    2 done

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,58 +1,79 @@
-// import flatpickr from 'flatpickr';
-// import 'flatpickr/dist/flatpickr.min.css';
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import Notiflix from 'notiflix';
 
-// const selector = document.querySelector('#datetime-picker');
-// const button = document.querySelector('[data-start]');
-// const date = new Date();
-// let deltaTime = 0;
+const selector = document.querySelector('#datetime-picker');
+const button = document.querySelector('[data-start]');
+const daysEl = document.querySelector('span[data-days]');
+const hoursEl = document.querySelector('span[data-hours]');
+const minutesEl = document.querySelector('span[data-minutes]');
+const secondsEl = document.querySelector('span[data-seconds]');
+const date = new Date();
+let deltaTime = 0;
+const PROMPT_DELAY = 1000;
 
-// button.disabled = true;
-// button.addEventListener('click', onBtnStart);
+button.disabled = true;
+button.addEventListener('click', onBtnStart);
 
-// const options = {
-//   enableTime: true,
-//   time_24hr: true,
-//   defaultDate: new Date(),
-//   minuteIncrement: 1,
-//   //   onClose(selectedDates) {
-//   //     console.log(selectedDates[0]);
-//   //   },
-//   onClose(selectedDates) {
-//     if (selectedDates[0] < date) {
-//       window.alert('Please choose a date in the future');
-//     }
-//     button.disabled = false;
-//   },
-// };
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  //   onClose(selectedDates) {
+  //     console.log(selectedDates[0]);
+  //   },
+  onClose(selectedDates) {
+    if (selectedDates[0] < date) {
+      Notiflix.Notify.failure('Please choose a date in the future');
+    } else {
+      button.disabled = false;
+    }
+  },
+};
 
-// flatpickr(selector, options);
+flatpickr(selector, options);
 
-// // console.log(new Date(selector.value));
+// console.log(new Date(selector.value));
 
-// function onBtnStart() {
-//   const futureDate = new Date(selector.value);
-//   deltaTime = futureDate.getTime() - date.getTime();
-//   return deltaTime;
-// }
+function onBtnStart() {
+  timerId = setInterval(() => {
+    const futureDate = new Date(selector.value);
+    const date = new Date();
+    deltaTime = convertMs(futureDate.getTime() - date.getTime());
+    if (futureDate.getTime() - date.getTime() <= 1000) {
+      clearInterval(timerId);
+      button.disabled = true;
+    }
 
-// function convertMs(ms) {
-//   // Number of milliseconds per unit of time
-//   const second = 1000;
-//   const minute = second * 60;
-//   const hour = minute * 60;
-//   const day = hour * 24;
+    console.log(deltaTime);
+    const { days, hours, minutes, seconds } = deltaTime;
+    daysEl.textContent = addLeadingZero(days);
+    hoursEl.textContent = addLeadingZero(hours);
+    secondsEl.textContent = addLeadingZero(seconds);
+    minutesEl.textContent = addLeadingZero(minutes);
+  }, PROMPT_DELAY);
+}
 
-//   // Remaining days
-//   const days = Math.floor(ms / day);
-//   // Remaining hours
-//   const hours = Math.floor((ms % day) / hour);
-//   // Remaining minutes
-//   const minutes = Math.floor(((ms % day) % hour) / minute);
-//   // Remaining seconds
-//   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
 
-//   return { days, hours, minutes, seconds };
-// }
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
-// convertMs(100000);
-// console.log('bb');
+  return { days, hours, minutes, seconds };
+}
+
+function addLeadingZero(param) {
+  return String(param).padStart(2, '0');
+}
