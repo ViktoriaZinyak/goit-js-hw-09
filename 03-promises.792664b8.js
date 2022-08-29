b8b60fac9560b4b5476b628ee514743c2e079a3f
ViktoriaZinyak <100145684+ViktoriaZinyak@users.noreply.github.com>
2022-08-29 08:29:01 +0000

    Deploying to gh-pages from @ ViktoriaZinyak/goit-js-hw-09@7a05c92b86eb40afe1775a275b10bb4958bf8b16 🚀

--- 03-promises.792664b8.js
+++
@@ -1,2 +0,0 @@
-var e,o;console.log("✅ Fulfilled promise in ms"),(e=2,o=1500,new Promise(((i,s)=>{const l=Math.random()>.3;setTimeout((()=>{l?i(`✅ Fulfilled promise ${e} in ${o}ms`):s(`❌ Rejected promise ${e} in ${o}ms`)}),o)}))).then((({position:e,delay:o})=>{console.log(`✅ Fulfilled promise ${e} in ${o}ms`)})).catch((({position:e,delay:o})=>{console.log(`❌ Rejected promise ${e} in ${o}ms`)}));
-//# sourceMappingURL=03-promises.792664b8.js.map
